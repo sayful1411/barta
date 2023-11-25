764e63c80ca50ebff82b67331edfe618bbf14146
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2023-11-25 16:07:59 +0600

    implemented eloquent model to profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
-use Illuminate\View\View;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\UserProfileRequest;
-use App\Http\Requests\UserSettingRequest;
-use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -20,46 +13,10 @@ class ProfileController extends Controller
     {
         $loggedInUserId = Auth::id();
 
-        // $posts = DB::table('posts')
-        //         ->join('users', 'posts.user_id', '=', 'users.id')
-        //         ->select('posts.*', 'users.fname as user_fname', 'users.lname as user_lname', 'users.username as user_username','users.email as user_email')
-        //         ->where('posts.user_id', $loggedInUserId)
-        //         ->orderBy('id','desc')
-        //         ->get();
-
-        $posts = DB::table('posts')
-            ->join('users', 'posts.user_id', '=', 'users.id')
-            ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-            ->select(
-                'posts.*',
-                'users.fname as user_fname',
-                'users.lname as user_lname',
-                'users.username as user_username',
-                'users.email as user_email',
-                DB::raw('COUNT(comments.id) as comment_count')
-            )
-            ->groupBy(
-                'posts.id',
-                'posts.uuid',
-                'posts.user_id',
-                'posts.description',
-                'posts.view_count',
-                'posts.image',
-                'posts.created_at',
-                'posts.updated_at',
-                'users.fname',
-                'users.lname',
-                'users.username',
-                'users.email'
-            )
+        $posts = Post::with(['user', 'comments'])
             ->where('posts.user_id', $loggedInUserId)
-            ->orderBy('posts.id', 'desc')
+            ->orderByDesc('created_at')
             ->get();
-
-        $posts = $posts->map(function ($post) {
-            $post->created_at = Carbon::parse($post->created_at);
-            return $post;
-        });
 
         return view("pages.profiles.profile", compact("posts"));
     }
@@ -81,18 +38,14 @@ class ProfileController extends Controller
         // get the authenticated user's id
         $userID = Auth::user()->id;
 
-        // Update the user's information
-        DB::table('users')
-            ->where('id', $userID)
-            ->update([
-                'fname' => $validated['fname'],
-                'lname' => $validated['lname'],
-                'email' => $validated['email'],
-                'bio' => $validated['bio'],
-                'updated_at' => now(),
-            ]);
+        User::where('id', $userID)->update([
+            'fname' => $validated['fname'],
+            'lname' => $validated['lname'],
+            'email' => $validated['email'],
+            'bio' => $validated['bio']
+        ]);
 
-        return redirect()->route('profile.edit')->with('success', 'User information updated successfully');
+        return redirect()->back()->with('success', 'User information updated successfully');
     }
 
     // public function destroy(Request $request): RedirectResponse
--- resources/views/pages/profiles/profile.blade.php
+++ resources/views/pages/profiles/profile.blade.php
@@ -91,11 +91,11 @@
                         <!-- User Info -->
                         <div class="text-gray-900 flex flex-col min-w-0 flex-1">
                             <a href="{{ route('profile') }}" class="hover:underline font-semibold line-clamp-1">
-                                {{ $post->user_fname . ' ' . $post->user_lname }}
+                                {{ $post->user->fname . ' ' . $post->user->lname }}
                             </a>
 
                             <a href="{{ route('profile') }}" class="hover:underline text-sm text-gray-500 line-clamp-1">
-                                {{ '@' . $post->user_username }}
+                                {{ '@' . $post->user->username }}
                             </a>
                         </div>
                         <!-- /User Info -->
@@ -199,7 +199,7 @@
 
             <!-- Date Created & View Stat -->
             <div class="flex items-center gap-2 text-gray-500 text-xs my-2">
-                <span class="">{{ $post->created_at->diffForHumans() }}</span>
+                <span class="">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</span>
                 <span class="">•</span>
                 <span>{{ $post->view_count }} views</span>
             </div>
@@ -220,7 +220,7 @@
                                 </path>
                             </svg>
 
-                            <p>{{ $post->comment_count }}</p>
+                            <p>{{ $post->comments->count() }}</p>
                         </a>
                         <!-- /Comment Button -->
                     </div>
