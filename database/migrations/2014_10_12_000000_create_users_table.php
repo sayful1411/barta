c64b8c53d8d9ff269a92e81adb1176a62cce3bab
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2023-12-02 12:49:50 +0600

    modify migration files and adding guest file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ return new class extends Migration
             $table->string('lname');
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('bio')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
--- database/migrations/2023_11_11_104701_create_posts_table.php
+++ database/migrations/2023_11_11_104701_create_posts_table.php
@@ -14,10 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->text('description');
             $table->integer('view_count')->default(0);
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
