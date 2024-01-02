c09f14b84d9dd44efb586ddb27ac4a617cb70f12
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2024-01-02 21:04:25 +0600

    implement queue

--- app/Notifications/PostComment.php
+++ app/Notifications/PostComment.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class PostComment extends Notification
+class PostComment extends Notification implements ShouldQueue
 {
     use Queueable;
 
