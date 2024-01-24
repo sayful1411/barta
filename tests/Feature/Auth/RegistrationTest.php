479f5e4aceac0689b1e45aa88e54e1c82eb0a570
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2024-01-24 12:37:16 +0600

    fix registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -10,12 +10,14 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $response = $this->post('/register', [
-        'name' => 'Test User',
+        'fname' => 'Test',
+        'lname' => 'User',
+        'username' => 'test_user',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(RouteServiceProvider::HOME);
+    $response->withSession('success');
+    $response->assertRedirect('/login');
 });
