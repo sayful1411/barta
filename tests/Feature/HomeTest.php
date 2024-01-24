a57eabe9e13b074cdd30c103d93dcbd125bece51
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2024-01-24 15:43:22 +0600

    home test done

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Mockery;
+
+it('can load the index page with posts', function () {
+
+    $user = User::factory()->create();
+    User::factory()->count(3)->create();
+    $posts = Post::factory()->count(5)->create();
+
+    // Mock the Post model to return the test data
+    $postMock = Mockery::mock(Post::class);
+    $postMock->shouldReceive('with')->with(['user.media', 'media', 'comments'])->andReturnSelf();
+    $postMock->shouldReceive('orderByDesc')->with('created_at')->andReturnSelf();
+    $postMock->shouldReceive('get')->andReturn($posts);
+
+    // dd($posts);
+
+    app()->instance(Post::class, $postMock);
+
+    $response = $this->actingAs($user)->get(route('index'));
+
+    $response->assertViewIs('index')
+        ->assertViewHas('posts', $posts);
+});
